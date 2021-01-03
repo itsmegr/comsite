ca832a41a149dd8cfebd266a42b828207d73287e
unigovind <unigovind@gmail.com>
2021-01-03 17:21:38 +0530

    added all before mysql

--- app.js
+++ app.js
@@ -3,14 +3,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-
+const db = require('./util/database');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+// db.execute('SELECT * FROM products')
+// .then(result=>{
+//     console.log(result[0][0]);
+// })
+// .catch(error=>{
+//     console.log(error);
+// });
+
 
 const error_404Handler =  require('./controllers/404');
-
-
 //app.use middlem runs every time when a request comes
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
---
+++ util/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host:'localhost',
+    user:'root',
+    database:'shopping-app',
+    password:'Govind@2000'
+})
+
+module.exports = pool.promise();
